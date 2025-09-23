8f73cad96f75cf59d96f2ecad3ce36d857cc1747
leotnjr <leotnjr@gmail.com>
2025-09-23 15:04:34 -0300

    Added main, edited structs in bmp_parser

--- wip/bmp_parser.c
+++ wip/bmp_parser.c
@@ -2,45 +2,43 @@
 #include<stdlib.h>
 #include<stdint.h>
 
-struct bmp
+
+// Contains general file information.
+struct header // 14 bytes.
 {
-    // Contains general file information.
-    struct header // 14 bytes.
-    {
-        unsigned char signature[2]; // 2 bytes
-        uint32_t fileSize;          // 4 bytes
-        uint32_t reservedBytes;     // 4 bytes
-        uint32_t arrayAddress;      // 4 bytes
-    };
-
-    // Contains specific file information.
-    struct infoHeader  // 40 bytes
-    {
-        uint32_t size;              // 4 bytes
-        int32_t imgWidth;           // 4 bytes
-        int32_t imgHeight;          // 4 bytes
-        uint16_t colorPlanes;       // 2 bytes
-        uint16_t bitPerPixel;       // 2 bytes
-        uint32_t compression;       // 4 bytes
-        uint32_t imgSize;           // 4 bytes
-        int32_t horizontalRes;      // 4 bytes
-        int32_t verticalRes;        // 4 bytes
-        uint32_t colorsInTable;     // 4 bytes
-        uint32_t importantColors;   // 4 bytes
-    };
-
-    // Image pixel array
-    struct pixelArray
-    {
-        char test;
-    };
+    unsigned char signature[2]; // 2 bytes
+    uint32_t fileSize;          // 4 bytes
+    uint32_t reservedBytes;     // 4 bytes
+    uint32_t arrayAddress;      // 4 bytes
 };
+
+// Contains specific file information.
+struct infoHeader  // 40 bytes
+{
+    uint32_t headerSize;         // 4 bytes
+    int32_t imgWidth;           // 4 bytes
+    int32_t imgHeight;          // 4 bytes
+    uint16_t colorPlanes;       // 2 bytes
+    uint16_t bitPerPixel;       // 2 bytes
+    uint32_t compression;       // 4 bytes
+    uint32_t imgSize;           // 4 bytes
+    int32_t horizontalRes;      // 4 bytes
+    int32_t verticalRes;        // 4 bytes
+    uint32_t colorsInTable;     // 4 bytes
+    uint32_t importantColors;   // 4 bytes
+};
+
+void bmp_ArrayAllocation()
+{
+
+}
+
 
 FILE *img;
 
 int main()
 {   
-    struct bmp image;
+    struct bmp *image;
 
     img = fopen("images/2x2.bmp", "rb");
     if (!img)
