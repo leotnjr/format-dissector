93485c3b5827e8d80475936fd79047ec46e0d384
leotnjr <leotnjr@gmail.com>
2025-09-29 11:33:58 -0300

    updating bmp_bytes, adding the general bmp file byte structrure

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
 # format-dissector
 Project overview
-Experiments dissecting image file formats in C, exploring headers, pixels, and byte-level structures.
+Experiments dissecting image file formats in C, exploring headers, pixels, and byte-level structures
+Trying to understand file formats and how to trasform them for deeper knowledge data management and manipulation.
+Experiment with raw data and have a solid foundation in how computers read and display information.
 
-Goals
-Understand file formats and how to trasform them for deeper data management.
-Experiment with raw data and have a solid foundation in how computers read and display information
+Current Goals
+-Make the bmp_bytes.exe displain in console the bitmap file data and the pixel array values.
+-Transform colored images in grayscale.
+-Make a simple picture-to-ASCII generator as proof of concept.
+-Transform a png file into a jpeg
+
+display the data of each pixel is
 
 This repository is experimental; code and structures are prototypes and may change.
 
--- wip/bmp_bytes.c
+++ wip/bmp_bytes.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
-#include<stdlib.h> 
+#include<stdlib.h>
+#include<stdint.h>
 
 /*
 This is my first attempt to understand bitmap format byte structure.
@@ -19,98 +20,23 @@ int main()
     
     // General info
     unsigned char buffer[500]; //max file size for now.
-    size_t file_size = fread(buffer, 1, 500, img);
+    // size_t file_size = fread(buffer, 1, 500, img);
 
-    printf("\nread bytes: %zu\n\n", file_size);
+
+    // printf("\nread bytes: %zu\n\n", file_size);
 
     //Shows which byte index have the contents of the image matrix  
-    int address = buffer[10];
-    printf("Image starts at byte %d\n\n",address);
+    //int address = buffer[10];
+    //printf("Image starts at byte %d\n\n",address);
 
     
     // Trying to see were each byte is and their contents
     // using byte offset found in the bmp format specifications.
-    for(i = 0; i < file_size; i++)
-    {
-        
-        switch (i)
-        {
-            case 0:
-                printf("\nHEADER"
-                    "\nFile format:"
-                    "\nByte %02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 2:
-                printf("\nFile size:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 6:
-                printf("\nReserved_A:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 8:
-                printf("\nReserved_B:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 10:
-                printf("\nStarting Address:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 14:
-                printf("\nBITMAPINFOREADER"
-                    "\nSize:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 18:
-                printf("\nWidth in pixels:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 22:
-                printf("\nHeight in pixels:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 26:
-                printf("\nNumber of color planes:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 28:
-                printf("\nBits per pixel:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 30:
-                printf("\nCompression method:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 34:
-                printf("\nImage size:"
-                    "\nByte:%02d: 0x%02X\n",
-                    i, buffer[i]);
-                break;
-            case 38:
-                printf("\nHorizontal resolution. Pixel per metre, signed integer:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 42:
-                printf("\nVertical resolution of the image. Pixel per metre, signed integer:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 46:
-                printf("\nNumber of colors in the color palette:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 50:
-                printf("\nNumber of important colors used, or 0 when every color is important:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            case 54:
-                printf("\nStart of Image:"
-                    "\nByte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-            default:
-                printf("Byte:%02d: 0x%02X\n", i, buffer[i]);
-                break;
-        }
-    }
+    unsigned char signature[3];
+    fread(signature, 1, 2, img);
+    signature[2] = '\0';
+    printf("\n\n\nThe signature is: %s.\n\n", signature);
+    
     getchar();
     
     fclose(img);
